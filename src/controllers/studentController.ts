5fcdf279eda82f722462635b41981291c834216a
kelvia sousa <kelviaks@gmail.com>
2021-06-01 20:50:32 -0300

    validação 1 e 2

--- src/controllers/studentController.ts
+++ src/controllers/studentController.ts
@@ -1,39 +1,59 @@
-import {Request, Response} from 'express'
-import student from '../types/student'
-import studentModel from '../model/studentModel'
-import {v4 as uuidv4} from 'uuid'
-const StudentController = {
-    create: async (req: Request, res: Response):Promise<any> => {
-        try{
-            const {name, birth_date, email}:student = req.body;
-            const id = uuidv4();
-            const dbResult = await studentModel.create({id, name, birth_date, email})
-            
-            if(dbResult !== 1){
-                 console.log(dbResult)
-                res.statusCode = 400;
-                throw new Error("Student not created")
-            }
-            res.send({message: "New student created"})
-        }
-        catch(err){
-            res.send({message: err.message})
-        }
-    },
-    getAgeById: async (req: Request, res: Response):Promise<any> => {
-        try{
-            const {id} = req.params;
+import { Request, Response } from "express";
+import student from "../types/student";
+import studentModel from "../model/studentModel";
+import { v4 as uuidv4 } from "uuid";
 
-            const dbResult = await studentModel.getAgeByID(id)
-            if(!dbResult.length){
-                res.statusCode = 400;
-                throw new Error("Student not found")
-            }
-            res.send(dbResult[0])
-        }
-        catch(err){
-            res.send({message: err.message})
-        }
+const StudentController = {
+  create: async (req: Request, res: Response): Promise<any> => {
+    try {
+      let { name, birth_date, email }: student = req.body;
+      name = name.trim();
+      if (!name) {
+        throw new Error("the name needs to be informed.");
+      }
+      if (!birth_date) {
+        throw new Error("the birth date needs to be informed.");
+      }
+      const date = new Date(birth_date).getTime();
+      if (isNaN(date)) {
+        throw new Error("Date isn't in a valid format");
+      }
+      email = email.trim();
+      if (!email) {
+        throw new Error("the email needs to be informed.");
+      }
+
+      const id = uuidv4();
+      const dbResult = await studentModel.create({
+        id,
+        name,
+        birth_date,
+        email,
+      });
+
+      if (dbResult !== 1) {
+        console.log(dbResult);
+        res.statusCode = 400;
+        throw new Error("Student not created");
+      }
+      res.send({ message: "New student created" });
+    } catch (err) {
+      res.send({ message: err.message });
     }
-} 
-export default StudentController
+  },
+  getAgeById: async (req: Request, res: Response): Promise<any> => {
+    try {
+      const { id } = req.params;
+
+      const dbResult = await studentModel.getAgeByID(id);
+      if (!dbResult.length) {
+        res.statusCode = 400;
+        throw new Error("Student not found");
+      }
+      res.send(dbResult[0]);
+    } catch (err) {
+      res.send({ message: err.message });
+    }
+  },
+};
+export default StudentController;
--- src/controllers/teacherController.ts
+++ src/controllers/teacherController.ts
@@ -1,24 +1,44 @@
-import {Request, Response} from 'express'
-import teacher from '../types/teacher'
-import teacherModel from '../model/teacherModel'
-import {v4 as uuidv4} from 'uuid'
+import { Request, Response } from "express";
+import teacher from "../types/teacher";
+import teacherModel from "../model/teacherModel";
+import { v4 as uuidv4 } from "uuid";
+
 const teacherController = {
-    create: async (req: Request, res: Response):Promise<any> => {
-        try{
-            const {name, birth_date, email}:teacher = req.body;
-            const id = uuidv4();
-            const dbResult = await teacherModel.create({id, name, birth_date, email})
-            
-            if(dbResult !== 1){
-                 console.log(dbResult)
-                res.statusCode = 400;
-                throw new Error("teacher not created")
-            }
-            res.send({message: "New teacher created"})
-        }
-        catch(err){
-            res.send({message: err.message})
-        }
+  create: async (req: Request, res: Response): Promise<any> => {
+    try {
+      let { name, birth_date, email }: teacher = req.body;
+      name = name.trim();
+      if (!name) {
+        throw new Error("the name needs to be informed.");
+      }
+      if (!birth_date) {
+        throw new Error("the birth date needs to be informed.");
+      }
+      const date = new Date(birth_date).getTime();
+      if (isNaN(date)) {
+        throw new Error("Date isn't in a valid format");
+      }
+      email = email.trim();
+      if (!email) {
+        throw new Error("the email needs to be informed.");
+      }
+      const id = uuidv4();
+      const dbResult = await teacherModel.create({
+        id,
+        name,
+        birth_date,
+        email,
+      });
+
+      if (dbResult !== 1) {
+        console.log(dbResult);
+        res.statusCode = 400;
+        throw new Error("teacher not created");
+      }
+      res.send({ message: "New teacher created" });
+    } catch (err) {
+      res.send({ message: err.message });
     }
-} 
-export default teacherController
+  },
+};
+export default teacherController;
